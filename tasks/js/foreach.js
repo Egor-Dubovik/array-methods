a71d9479044130d20b89fbe8406546ada7a67799
Egor Dubovik <egorka54896@gmail.com>
2022-07-04 17:11:07 +0300

    feat: add solution for task 5 (forEach)

--- tasks/js/foreach.js
+++ tasks/js/foreach.js
@@ -7,25 +7,55 @@
 // В новом массиве оставьте только 3 поля: name, id и title
 
 //-----------------------------------------------------------------------------------
-import { movieLists } from "./database.js";
+import { movieLists, boxarts } from "./database.js";
 
-const ratingFiltering = (movieLists) => {
-  let filteringVideos = [];
+//* O(n^2)
+// const ratingFiltering = (movieLists) => {
+//   let filteringVideos = [];
 
-  movieLists.forEach(list => {
-    list.videos.forEach(video => {
-      if (video.rating == 5.0) {
-        const name = list.name;
-        const { id, title } = video;
-        filteringVideos.push({ name, id, title });
+//   movieLists.forEach(list => {
+//     list.videos.forEach(video => {
+//       if (video.rating == 5.0) {
+//         const name = list.name;
+//         const { id, title } = video;
+//         filteringVideos.push({ name, id, title });
+//       }
+//     })
+//   });
+
+//   return filteringVideos;
+// };
+
+
+// const videos = ratingFiltering(movieLists);
+// console.log(videos);
+//========================================================================================================================================================
+
+// Задача #5
+
+// Ограничения: выполните задание используя forEach
+// Задание: достать объект с самой большой площадью картинки
+
+const showoLargestObject = (boxarts) => {
+  let arr = [];
+
+  boxarts.forEach(el => {
+    if (arr.length === 0) {
+      arr.push(el);
+    } else {
+      const currentSquare = el.width * el.height;
+      const biggestSquare = arr[0].width * arr[0].height;
+
+      if (currentSquare > biggestSquare) {
+        arr.length = 0;
+        arr.push(el);
       }
-    })
-
+    }
   });
 
-  return filteringVideos;
-};
+
+  return arr[0];
+}
 
 
-const videos = ratingFiltering(movieLists);
-console.log(videos);
+console.log(showoLargestObject(boxarts));
